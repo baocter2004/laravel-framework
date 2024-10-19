7b5b3ef8eba3e376a62532535846f554efff3fcc
baocter <be93700@gmail.com>
2024-10-19 09:48:32 +0700

    Buổi 5 Controllers

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,9 @@ class UserController extends Controller
      */
     public function index()
     {
-        echo "đây là trang chủ nè";
+        $data = User::latest('id')->paginate(10);
+
+        return view('users.index',['data'=>$data]);
     }
 
     /**
@@ -21,7 +23,7 @@ class UserController extends Controller
      */
     public function create()
     {
-        //
+        return view('users.create');
     }
 
     /**
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('role')->default(User::ROLE_MEMBER);
+            $table->string('image')->nullable();
+            $table->boolean('is_active')->default(1);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
